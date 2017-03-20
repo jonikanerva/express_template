370c5c6f68f9230d725a31f0e2490b6fc42beef2
Joni Kanerva <joni@devlab.fi>
2017-03-20 18:05:10 +0200

    Add logging to development/test environment

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 // Add dependencies
 const app = require('express')()
 const helmet = require('helmet')
+const morgan = require('morgan')
+const bodyparser = require('body-parser')
 const routes = require('./routes/routes')
 const config = require('./config/config')
+
+// Set verbose logger for development environment
+if (config.environment !== 'production') {
+  app.use(bodyparser.json())
+  app.use(bodyparser.urlencoded({ extended: true }))
+
+  morgan.token('body', (req, res) => JSON.stringify(req.body))
+
+  app.use(morgan(':method :url :status :response-time ms :body'))
+}
 
 // Set HTTP headers
 app.use(helmet())
