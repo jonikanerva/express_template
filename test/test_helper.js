c3fb625d7a8a370406dc47fb083636458c437d0f
Joni Kanerva <joni@devlab.fi>
2017-03-20 15:46:43 +0200

    Add comment

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,3 +1,6 @@
+// This file contains the configuration of the test framework.
+// you should include this file and import the exported functions in your tests
+
 const chai = require('chai')
 const app = require('../app')
 const assert = chai.assert
