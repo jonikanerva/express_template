176139e38486aad82c79fd2dccdaba25b4929054
Joni Kanerva <joni@devlab.fi>
2017-03-20 12:53:42 +0200

    Different configs for production/development

--- config/config.js
+++ config/config.js
@@ -1,7 +1,21 @@
 require('dotenv').config()
 
-module.exports = {
+const all = {
+  environment: process.env.NODE_ENV || 'development'
+}
+
+const development = {
   port: process.env.PORT || 3000,
-  environment: process.env.NODE_ENV || 'development',
   database_url: process.env.DATABASE_URL || 'postgres://foo:bar@localhost/express_api'
 }
+
+const production = {
+  port: process.env.PORT || 5000,
+  database_url: process.env.DATABASE_URL
+}
+
+const config = (all.environment === 'production')
+  ? Object.assign(all, production)
+  : Object.assign(all, development)
+
+module.exports = config
