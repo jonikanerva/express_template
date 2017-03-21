b7f95da1afbb7c6ea558075d6d6007da57c78d27
Joni Kanerva <joni@devlab.fi>
2017-03-21 09:44:35 +0200

    Only get

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const routes = require('express').Router()
 const foo = require('./foo')
 const root = require('./root')
 
-routes.all('/foo', foo)
-routes.all('/', root)
+routes.get('/foo', foo)
+routes.get('/', root)
 
 module.exports = routes
