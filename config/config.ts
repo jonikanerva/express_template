7ffe4582be7900dd33af349adcb180ae0f53badf
Joni Kanerva <jhonny@jhonny.org>
2019-02-02 14:38:49 +0200

    cleanup

--- config/config.ts
+++ config/config.ts
@@ -11,18 +11,10 @@ interface Config {
   environment: string
   port: string
   logLevel: string
-  feedbin: {
-    username: string
-    password: string
-  }
 }
 
 export const config: Config = {
   environment,
-  feedbin: {
-    username: process.env.FEEDBIN_USER || '',
-    password: process.env.FEEDBIN_PASS || ''
-  },
   logLevel: environment === 'local' ? 'silly' : 'info',
   port: process.env.PORT || '3000'
 }
